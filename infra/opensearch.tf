6f9a10fc09ce4266a7034f6a7bab3cdd69851ebc
SungChul23 <kimsam0923@naver.com>
2026-08-28 15:58:35 +0900

    docs: opensearch.tf에서 주석 처리된 provisioner 블록 수정

--- infra/opensearch.tf
+++ infra/opensearch.tf
@@ -64,11 +64,11 @@ resource "aws_opensearch_domain" "factory" {
   # 데이터 구조가 변경되면 -> 교체
   # 이런 정보가 없으면 opensearch가 데이터를 보고 -> 타입추정 -> 타입 매칭
   # 주로 timestamp 에서 오류 발생이 많음
-  provisioner "local-exec" {
-    command = <<-EOT
-      curl -X PUT "https://${self.endpoint}/factory-sensor" \
-        -H "Content-Type: application/json" \
-        -d '{"mappings":{"properties":{"@timestamp":{"type":"date_nanos"},"timestamp":{"type":"date"},"vector_ingest_at":{"type":"date_nanos"},"sensor_id":{"type":"keyword"},"temperature":{"type":"float"},"humidity":{"type":"float"},"status":{"type":"keyword"}}}}'
-    EOT
-  }
+#   provisioner "local-exec" {
+#     command = <<-EOT
+#       curl -X PUT "https://${self.endpoint}/factory-sensor" \
+#         -H "Content-Type: application/json" \
+#         -d '{"mappings":{"properties":{"@timestamp":{"type":"date_nanos"},"timestamp":{"type":"date"},"vector_ingest_at":{"type":"date_nanos"},"sensor_id":{"type":"keyword"},"temperature":{"type":"float"},"humidity":{"type":"float"},"status":{"type":"keyword"}}}}'
+#     EOT
+#   }
 }
